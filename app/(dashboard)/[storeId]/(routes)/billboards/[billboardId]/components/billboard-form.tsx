ab2b7d1f4d6927838effbe5ea04595adfbf23d3c
Kevin Renner <130848157+kevinandrerenner@users.noreply.github.com>
2023-09-26 14:42:23 +0200

    billboard search implemented

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -23,8 +23,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { AlertModal } from "@/components/modals/alert-modal";
-import { ApiAlert } from "@/components/ui/api-alert";
-import { useOrigin } from "@/hooks/use-origin";
+import ImageUpload from "@/components/ui/image-upload";
 
 const formSchema = z.object({
   label: z.string().min(1),
@@ -40,14 +39,13 @@ interface BillbordFormProps {
 export const BillbordForm: React.FC<BillbordFormProps> = ({ initialData }) => {
   const params = useParams();
   const router = useRouter();
-  const origin = useOrigin();
 
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const title = initialData ? "Edit billboard" : "Create billboard";
   const description = initialData ? "Edit a billboard" : "Add a new billboard";
-  const toastMessage = initialData ? "Edit billboard" : "Billboard created.";
+  const toastMessage = initialData ? "Billboard updated" : "Billboard created.";
   const action = initialData ? "Save changes" : "Create";
 
   const form = useForm<BillbordFormValues>({
@@ -61,9 +59,14 @@ export const BillbordForm: React.FC<BillbordFormProps> = ({ initialData }) => {
   const onSubmit = async (data: BillbordFormValues) => {
     try {
       setLoading(true);
-      await axios.patch(`/api/stores/${params.storeId}`, data);
+      if (initialData) {
+        await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data);
+      } else {
+        await axios.post(`/api/${params.storeId}/billboards`, data);
+      } 
       router.refresh();
-      toast.success("Store updated");
+      router.push(`/${params.storeId}/billboards`)
+      toast.success(toastMessage);
     } catch (error) {
       toast.error("Something went wrong.");
     } finally {
@@ -74,12 +77,12 @@ export const BillbordForm: React.FC<BillbordFormProps> = ({ initialData }) => {
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/stores/${params.storeId}`);
+      await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
       router.refresh();
       router.push("/");
-      toast.success("Store deleted");
+      toast.success("Billboard deleted");
     } catch (error) {
-      toast.error("Make sure you removed all products and categories first.");
+      toast.error("Make sure you removed categories using this billboard first.");
     } finally {
       setLoading(false);
       setOpen(false);
@@ -113,6 +116,24 @@ export const BillbordForm: React.FC<BillbordFormProps> = ({ initialData }) => {
           onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-8 w-full"
         >
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Background image</FormLabel>
+                <FormControl>
+                  <ImageUpload 
+                    value={field.value ? [field.value] : []}
+                    disabled={loading}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange("")}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <div className="grid gid-cols-3 gap-8">
             <FormField
               control={form.control}
--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,29 +1,39 @@
-"use client"
+"use client";
 
-import { Plus } from "lucide-react"
-import { useParams, useRouter } from "next/navigation"
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
 
-import { Button } from "@/components/ui/button"
-import { Heading } from "@/components/ui/heading"
-import { Separator } from "@/components/ui/separator"
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 
-export const BillboardClient = () => {
-    const router = useRouter();
-    const params = useParams();
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
 
-    return (
-        <>
-            <div className="flex items-center justify-between">
-                <Heading 
-                    title="Billboards (0)"
-                    description="Manage billboards for your store"
-                />
-                <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
-                    <Plus className="mr-2 h-4 w-4" />
-                    Add new
-                </Button>
-            </div>
-            <Separator />
-        </>
-    )
+interface BillboardClientProps {
+  data: BillboardColumn[];
 }
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Billboards (${data.length})`}
+          description="Manage billboards for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add new
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="label" columns={columns} data={data} />
+    </>
+  );
+};
